613a1359c908e05e15cba9cd43a17281272f3fe3
Ana Lemos <anapaulalazzarottodelemos@Anas-MacBook-Pro.local>
2023-04-18 16:58:22 -0300

    Use older next version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+const isGithubActions = process.env.GITHUB_PUBLIC_BASE_PATH || false;
+
+// GITHUB_REPOSITORY is <owner>/<repo>, so we need to trim off `<owner>/`
+const prefix = isGithubActions
+  ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}`
+  : undefined;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'dist',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: prefix,
+  assetPrefix: prefix,
 }
 
 module.exports = nextConfig
